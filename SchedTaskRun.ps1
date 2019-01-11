673c6bf57f3f38255e0e5d0632f5d49da50d63b4
SirConfigMgr <43637014+SirConfigMgr@users.noreply.github.com>
2019-01-11 08:03:01 +0100

    Delete SchedTaskRun.ps1

--- SchedTaskRun.ps1
+++
@@ -1,7 +0,0 @@
-### Import Config File ################################################################
-. "$PSScriptRoot\Config.ps1"
-#######################################################################################
-
-New-Item -Path $WorkFolder\$InplaceVersion\StartBySchedTask.txt
-Get-WmiObject -Namespace "root\ccm\scheduler" -Class ccm_scheduler_history | where {$_.ScheduleID -like "*$TSPackageID*"} | Remove-WmiObject
-Get-Service | where {$_.Name -eq "CCMExec"} | Restart-Service
